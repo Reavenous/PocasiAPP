8ef3a264d6cf5163f8911679d776fec0f1d0675e
Alexandr <basseville2000@gmail.com>
2024-05-10 19:41:35 +0200

    vytvoření třídy launcher

--- src/PocasiAplikace.java
+++ src/PocasiAplikace.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class PocasiAplikace {
+import javax.swing.*;
+
+public class PocasiAplikace extends JFrame {
+
+    public PocasiAplikace(){
+        super("Počasí");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,750);
+        setLocationRelativeTo(null);
+        setLayout(null);
+        setResizable(false);
+    }
 }
