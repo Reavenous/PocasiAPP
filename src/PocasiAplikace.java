95872ab77adc55b2fac89c8417ab727120d703cc
Alexandr <basseville2000@gmail.com>
2024-05-10 19:56:31 +0200

    Vytvoření launcheru na JFrame

--- src/PocasiAplikace.java
+++ src/PocasiAplikace.java
@@ -5,7 +5,7 @@ public class PocasiAplikace extends JFrame {
     public PocasiAplikace(){
         super("Počasí");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(500,750);
+        setSize(650,750);
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
