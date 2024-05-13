8a333f7938bbbdd20e36bbbd0f87656292654e0e
Alexandr <basseville2000@gmail.com>
2024-05-13 21:18:39 +0200

    nastavení funkce na tlačítko vyhledávání

--- src/PocasiAplikace.java
+++ src/PocasiAplikace.java
@@ -1,4 +1,10 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
 
 public class PocasiAplikace extends JFrame {
 
@@ -9,5 +15,24 @@ public class PocasiAplikace extends JFrame {
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
+        addComponents();
+    }
+
+    private void addComponents(){
+        JTextField searchTextField = new JTextField();
+        searchTextField.setBounds(25,25,400,75);
+        searchTextField.setFont(new Font("Dialog",Font.ROMAN_BASELINE,27));
+        add(searchTextField);
+        JButton searchB = new JButton(loadImage("pocasiFoto/search.png"));
+    }
+    private ImageIcon loadImage(String resourcePath){
+        try {
+            BufferedImage image = ImageIO.read(new File(resourcePath));
+            return new ImageIcon(image);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        System.out.println("resource not found!");
+        return null;
     }
 }
