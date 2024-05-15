1f3d349bbb829f163c3594b62e55d040233bbb3f
Alexandr <basseville2000@gmail.com>
2024-05-15 22:13:34 +0200

    pokračování ve vytváření potřebných Jlabelů

--- src/PocasiAplikace.java
+++ src/PocasiAplikace.java
@@ -23,6 +23,7 @@ public class PocasiAplikace extends JFrame {
         searchTextField.setBounds(50,25,400,75);
         searchTextField.setFont(new Font("Dialog",Font.ROMAN_BASELINE,27));
         add(searchTextField);
+
         JButton searchB = new JButton(loadImage("src/pocasiFoto/search.png"));
         searchB.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         searchB.setBounds(455   ,25,75,75);
@@ -32,9 +33,15 @@ public class PocasiAplikace extends JFrame {
         weatherImage.setBounds(13,125,550,250);
         add(weatherImage);
 
+        JLabel whatWeather = new JLabel("cloudy ");
+        whatWeather.setBounds(13,400,550,45);
+        whatWeather.setFont(new Font("Dialog",Font.ROMAN_BASELINE,42));
+        whatWeather.setHorizontalAlignment(SwingConstants.CENTER);
+        add(whatWeather);
+
         JLabel temperatureTxt = new JLabel("10 C");
         temperatureTxt.setBounds(13,350,550,60);
-        temperatureTxt.setFont(new Font("dialog",Font.ROMAN_BASELINE,52));
+        temperatureTxt.setFont(new Font("Dialog",Font.ROMAN_BASELINE,52));
         temperatureTxt.setHorizontalAlignment(SwingConstants.CENTER);
         add(temperatureTxt);
     }
