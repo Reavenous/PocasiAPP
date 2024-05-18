f5741972ee9418f34f4cc44f8afcbad7baa15e68
Alexandr <basseville2000@gmail.com>
2024-05-18 20:30:27 +0200

    dodělání labelů pro rychlost větru a vlhkost

---
+++ src/Geolocation.java
@@ -0,0 +1,12 @@
+import netscape.javascript.JSObject;
+
+public class Geolocation {
+
+    public static JSObject getWData(String locName){
+        JSONArray locData = getWData(locName);
+    }
+    private static JSONArray getLocData(String locName){
+        locName = locName.replaceAll(" ","+");
+        String url = ""
+    }
+}
--- src/PocasiAplikace.java
+++ src/PocasiAplikace.java
@@ -11,7 +11,7 @@ public class PocasiAplikace extends JFrame {
     public PocasiAplikace(){
         super("Počasí");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setSize(650,750);
+        setSize(600,900);
         setLocationRelativeTo(null);
         setLayout(null);
         setResizable(false);
@@ -26,19 +26,61 @@ public class PocasiAplikace extends JFrame {
 
         JLabel temperatureTxt = new JLabel("10 C");
         temperatureTxt.setBounds(13,350,550,60);
-        temperatureTxt.setFont(new Font("Dialog",Font.ROMAN_BASELINE,52));
+        temperatureTxt.setFont(new Font("Dialog",Font.BOLD,52));
         temperatureTxt.setHorizontalAlignment(SwingConstants.CENTER);
         add(temperatureTxt);
 
         JLabel whatWeather = new JLabel("cloudy ");
         whatWeather.setBounds(13,400,550,45);
-        whatWeather.setFont(new Font("Dialog",Font.ROMAN_BASELINE,42));
+        whatWeather.setFont(new Font("Dialog",Font.BOLD,42));
         whatWeather.setHorizontalAlignment(SwingConstants.CENTER);
         add(whatWeather);
 
+        JLabel humidityTxt = new JLabel("100%");
+        humidityTxt.setBounds(62,400,75,75);
+        humidityTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(humidityTxt);
+
+        JLabel uvIndexTxt = new JLabel("5");
+        uvIndexTxt.setBounds(75,500,25,75);
+        uvIndexTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(uvIndexTxt);
+
+        JLabel windForceTxt = new JLabel("100 km/h");
+        windForceTxt.setBounds(435,400,200,75);
+        windForceTxt.setFont(new Font("dialog",Font.BOLD, 20));
+        add(windForceTxt);
+
+        JLabel pressureTxt =new JLabel("2500 ph");
+        pressureTxt.setBounds(250,500,200,75);
+        pressureTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(pressureTxt);
+
+        JLabel precipitationsTxt = new JLabel("2500 mm");
+        precipitationsTxt.setBounds(435,500,200,75);
+        precipitationsTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(precipitationsTxt);
+
+        JLabel sunriseTxt = new JLabel("06:99");
+        sunriseTxt.setBounds(70,600,200,75);
+        sunriseTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(sunriseTxt);
+
+        JLabel sunsetTxt = new JLabel("21:96");
+        sunsetTxt.setBounds(250,600,200,75);
+        sunsetTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(sunsetTxt);
+
+        JLabel moonStateTxt = new JLabel("new moon");
+        moonStateTxt.setBounds(435,600,200,75);
+        moonStateTxt.setFont(new Font("dialog",Font.BOLD,20));
+        add(moonStateTxt);
+
+
+
         JButton searchB = new JButton(loadIM("src/pocasiFoto/search.png"));
         searchB.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
-        searchB.setBounds(455   ,25,75,75);
+        searchB.setBounds(455,25,75,75);
         add(searchB);
 
         JLabel weatherIM = new JLabel(loadIM("src/pocasiFoto/cloudy.png"));
@@ -48,6 +90,36 @@ public class PocasiAplikace extends JFrame {
         JLabel humidityIM = new JLabel(loadIM("src/pocasiFoto/humidity.png"));
         humidityIM.setBounds(50,350,75,75);
         add(humidityIM);
+
+        JLabel windForceIM = new JLabel(loadIM("src/pocasiFoto/windspeed.png"));
+        windForceIM.setBounds(435,350,75,75);
+        add(windForceIM);
+
+        JLabel uvIndexIM = new JLabel(loadIM("src/pocasiFoto/search.png"));
+        uvIndexIM.setBounds(50,450,75,75);
+        add(uvIndexIM);
+
+        JLabel pressureIM = new JLabel(loadIM("src/pocasiFoto/search.png"));
+        pressureIM.setBounds(250,450,75,75);
+        add(pressureIM);
+
+        JLabel precipitationIM = new JLabel("src/pocasiFoto/search.png");
+        precipitationIM.setBounds(435,450,75,75);
+        add(precipitationIM);
+
+        JLabel sunriseIM = new JLabel("src/pocasiFoto/search.png");
+        sunriseIM.setBounds(50,550,75,75);
+        add(sunriseIM);
+
+        JLabel sunsetIM = new JLabel("src/pocasiFoto/search.png");
+        sunsetIM.setBounds(250,550,75,75);
+        add(sunsetIM);
+
+        JLabel moonStateIM = new JLabel("src/pocasiFoto/search.png");
+        moonStateIM.setBounds(435,550,75,75);
+        add(moonStateIM);
+
+
     }
     private ImageIcon loadIM(String resourcePath){
         try {
