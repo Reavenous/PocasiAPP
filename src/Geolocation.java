ac9d28852430ace5eff8c33ea2a1b9d405f1b845
Alexandr <basseville2000@gmail.com>
2024-05-26 14:20:36 +0200

    získání geolokace dodělání

--- src/Geolocation.java
+++ src/Geolocation.java
@@ -1,6 +1,8 @@
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
@@ -11,40 +13,53 @@ public class Geolocation {
 
    public static JSONObject getWData(String locName){
         JSONArray locData = getLocData(locName);
+        return null;
     }
-    private static JSONArray getLocData(String locName){
+    public static JSONArray getLocData(String locName){
         locName = locName.replaceAll(" ","+");
         String urlS = "https://geocoding-api.open-meteo.com/v1/search?name=" +
                 locName + "&count=10&language=en&format=json";
 
         try {
             HttpURLConnection  connection = fetchApiResponse(urlS);
+            if (connection.getResponseCode() != 200){
+                System.out.println("Couldn't connect to API");
+                return null;
+            }else{
+                StringBuilder resultJ =new StringBuilder();
+                Scanner sc = new Scanner(connection.getInputStream());
 
+                while (sc.hasNext()){
+                    resultJ.append(sc.nextLine());
+                }
+                sc.close();
+                connection.disconnect();
+
+                JSONParser parser = new JSONParser();
+                JSONObject resultJOBJ = (JSONObject) parser.parse(String.valueOf(resultJ));
+                JSONArray locData = (JSONArray) resultJOBJ.get("results");
+                return locData;
+            }
         }catch (Exception e){
             e.printStackTrace();
         }
-    }
+        return null;
+   }
     private static HttpURLConnection fetchApiResponse(String urlS){
        try {
            URL url = new URL(urlS);
            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 
-           if (connection.getResponseCode() != 200){
-               System.out.println("Couldn't connect to API");
-               return null;
-           }else{
-            StringBuilder resultJ =new StringBuilder();
-            Scanner sc = new Scanner(connection.getInputStream());
-            while (sc.hasNext()){
-                resultJ.append(sc.nextLine());
-            }
-           }
            connection.setRequestMethod("GET");
            connection.connect();
            return connection;
-       }catch (IOException e ){
+
+       }catch(IOException e){
            e.printStackTrace();
        }
-       return null;
+
+
+        return null;
     }
+
 }
