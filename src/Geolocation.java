ed723965261db8fe1dd0cacbb4293c54893bfb9c
Alexandr <basseville2000@gmail.com>
2024-05-27 19:39:10 +0200

    zaerátek sbírání dat o počasí

--- src/Geolocation.java
+++ src/Geolocation.java
@@ -13,6 +13,26 @@ public class Geolocation {
 
    public static JSONObject getWData(String locName){
         JSONArray locData = getLocData(locName);
+
+        JSONObject location =  (JSONObject) locData.get(0);
+        double latitude = (double) location.get("latitude");
+        double longitude = (double) location.get("longitude");
+
+        String urlStr = "https://api.open-meteo.com/v1/forecast?" +
+                "latitude=" + latitude +"&longitude="+ longitude+
+                "&hourly=temperature_2m,relative_humidity_2m,weather_code,surface_pressure,wind_speed_10m&daily=weather_code,sunrise,sunset,uv_index_max,precipitation_sum&timezone=Europe%2FBerlin&forecast_days=1";
+        try {
+            HttpURLConnection connection = fetchApiResponse(urlStr);
+            if (connection.getResponseCode() != 200){
+                System.out.println("Couldn't connect to api");
+                return null;
+            }
+            StringBuilder resultJ = new StringBuilder();
+            Scanner sc = new Scanner(connection.getInputStream());
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
         return null;
     }
     public static JSONArray getLocData(String locName){
